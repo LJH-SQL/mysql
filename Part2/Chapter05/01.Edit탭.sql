7f048e70cdf5698a614a5686b863227f8190ff7a
luyer1325 <76413956+luyer1325@users.noreply.github.com>
2021-09-10 16:08:23 +0900

    Add files via upload

---
+++ Part2/Chapter05/01.Edit탭.sql
@@ -0,0 +1,10 @@
+/*
+Edit - Format - UPCASE Keywords or lowercase Keywords		-> 예약어 모두 대문자 or 소문자로 변경
+Edit - Format - Beautify Query		-> 쿼리문 표준형태로 재배열
+Edit - Preferences - SQL Editor - Query Editor - >>	USE UPPERCASE keywords on completion 체크		-> 예약더 자동완성 대문자로 나옴 
+*/
+USE shopDB;
+SELECT 
+    *
+FROM
+    memberTBL;
