ef367d6078574bffdb0b63c3f818f6a8641c20e2
luyer1325 <76413956+luyer1325@users.noreply.github.com>
2021-11-25 00:24:26 +0900

    Add files via upload

--- Part2/Chapter07/4.피벗.sql
+++ Part2/Chapter07/4.피벗.sql
@@ -10,10 +10,41 @@ CREATE TABLE pivotTest
 INSERT INTO pivotTest VALUES ('김범수','겨울',10), ('윤종신','여름',15), ('김범수','가을',25),('김범수','봄',3),('김범수','봄',37),('윤종신','겨울',40),('김범수','여름',14),
 ('김범수','겨울',22), ('윤종신','여름',64);
 SELECT * FROM pivotTest;
+/*출력문
+	uName	season	amount
+	김범수	겨울		10
+	윤종신	여름		15
+	김범수	가을		25
+	김범수	봄		3
+	김범수	봄		37
+	윤종신	겨울		40
+	김범수	여름		14
+	김범수	겨울		22
+	윤종신	여름		64
+*/
 
 SELECT uName,
 	SUM(IF(season='봄', amount, 0)) AS '봄',
     SUM(IF(season='여름', amount, 0)) AS '여름',
     SUM(IF(season='가을', amount, 0)) AS '가을',
     SUM(IF(season='겨울', amount, 0)) AS '겨울',
-    SUM(amount) AS '합계' FROM pivotTest GROUP BY uName;
+    SUM(amount) AS '합계' FROM pivotTest GROUP BY uName; 
+/*출력문
+uName	봄	여름	가을	겨울	합계
+김범수	40	14	25	32	111
+윤종신	0	79	0	40	119
+*/
+
+
+/*퀴즈
+	season	김범수	윤종신	합계
+	가을		25		0		25
+	봄		40		0		40
+	겨울		32		40		72
+	여름		14		79		93
+    위 출력문을 뽑아보시오
+*/
+SELECT season,
+	SUM(IF(uname='김범수',amount,0)) AS '김범수',
+    SUM(IF(uname='윤종신',amount,0)) AS '윤종신',
+    SUM(amount) AS '합계'  FROM pivotTest GROUP BY season ORDER BY 합계 ASC;
